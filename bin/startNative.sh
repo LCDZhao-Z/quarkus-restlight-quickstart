788402b916e61a9ab6eea43e1156590e61280a64
LCDZhao-O <weizhao@oppo.com>
2021-12-27 14:34:18 +0800

    modify startNative.sh

--- bin/startNative.sh
+++ bin/startNative.sh
@@ -1,3 +1,3 @@
 time=$(date "+%Y-%m-%d %H:%M:%S")
 echo "${time}: begin start"
-./target/quarkus-restlight-quickstart-1.0.0-SNAPSHOT-runner
+./../target/quarkus-restlight-quickstart-1.0.0-SNAPSHOT-runner
