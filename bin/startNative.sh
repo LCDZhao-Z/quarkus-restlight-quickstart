8811d8e270fe5052144b8cc92a1bd26e9dd76e80
LCDZhao-O <weizhao@oppo.com>
2021-12-27 14:59:51 +0800

    modify dataFormat of startNative.sh

--- bin/startNative.sh
+++ bin/startNative.sh
@@ -1,3 +1,3 @@
-time=$(date "+%Y-%m-%d %H:%M:%S.%N")
+time=$(date "+%Y-%m-%d %H:%M:%S").$((`date "+%N"`/1000000))
 echo "${time}: begin start"
 ./../target/quarkus-restlight-quickstart-1.0.0-SNAPSHOT-runner
